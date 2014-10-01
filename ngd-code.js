3fa88e0074610b82ab0f75a2d5b4172bfaf68b84
Allen Kim <allenhwkim@gmail.com>
2014-10-01 17:02:17 -0400

    adding ngd-code

---
+++ ngd-code.js
@@ -0,0 +1,20 @@
+/**
+ * builds the proper browser understanding code from human understanding code
+  */
+NGD = NGD || angular.module('ngd',[]);
+NGD.directive('ngdCode', function() {
+  return {
+    restrict: 'A',
+    link: function($scope, elem) {
+      var code = elem.html();
+      code = code.replace(/^[\r\n]/,"");    //remove the first linefeed
+      code = code.replace(/[\r\n]\s+$/,""); //remove the last linefeed 
+      var indent = code.match(/^\s+/);      //get the first indentation
+      if (indent) {                         //replace all leading indentation
+        var re = new RegExp("^"+indent, "gm");
+        code = code.replace(re, "");
+      }
+      elem.html(code);
+    }
+  };
+});
