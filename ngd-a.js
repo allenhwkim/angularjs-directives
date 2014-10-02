4e39ea83f951e4dcec21fa71f9dab3cfcf929477
Allen Kim <allenhwkim@gmail.com>
2014-10-02 16:47:31 -0400

    removing ngd-a.js

--- ngd-a.js
+++
@@ -1,8 +0,0 @@
-/* ng-include changes the default behaviour of "a href='#XXX'". This is a work-around */
-NGD.directive('ngdA', function() {
-  return {
-    link: function(scope, element) {
-      element.attr('href', '#' + element.attr('href'));
-    }
-  };
-});
