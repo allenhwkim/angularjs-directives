ccde64547a7a9fd1736317bfb7e93442b0c6d511
Allen Kim <allenhwkim@gmail.com>
2014-10-10 17:46:47 -0400

    Update ngd-script.js

--- ngd-script.js
+++ ngd-script.js
@@ -1,7 +1,7 @@
 var NGD = NGD || angular.module('ngd', []);
 NGD.directive('ngdScript', function() {
   return {
-    restrict: 'E',
+    restrict: 'AE',
     link: function(scope, element, attrs) {
       if (attr.type === 'text/javascript-lazy') {
         if( attrs.src !== undefined) {
